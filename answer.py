91bead676c116027f46a1080fef6933f2f5aed85
Kranti Sri Kalepalli <157083369+kkrantisri@users.noreply.github.com>
2024-11-13 11:00:34 -0500

    Initial commit

--- answer.py
+++ answer.py
@@ -1,38 +1,38 @@
 
+"""
+Questons 1:
+========================================================================================================
+For an OOP program about a Rectangle, please follow the instructions provided above each function.
+"""
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-def tuple():
-    """
-    This is to review tuples and basic operations.
-    """
-    # Assign the first element of the tuple "t1" to the variable x.
-    t1 = (13, 100, 99, 100, 999)
-
-
-    # Assign the next to the last element of the tuple "t1" to the
-    # variable y. For the given tuple, the next to the last element
-    # is 100.
-
-
-    # Determine the index of 2 in the tuple "t2" by using the index
-    # method for tuples. Assign the result to the variable z.
+    # This area function is used to return the area of a rectangle, use the rectangle function that area = width * height.
+    # Example: area for a rectangle(3,4) will be 3 * 4 = 12.
+    def area(self):
+        return 
     
-    t2 = (55, 777, 54, 6, 76, 101, 1, 2, 8679, 98, 123, 98, 98)
-
-    # Determine the number of times 98 occurs in the tuple "t2" by
-    # using the count method for tuples. Assign the result to
-    # the variable f.
+    # This perimeter function is used to return the perimeter of a rectangle.
+    # Example: preimeter for a rectangle(3,4) will be (3+4) * 2 = 14.
+    def perimeter(self):
+        return 
     
-
-    # Determine the sum of all the numbers in the tuple "t2" and
-    # assign the result to the variable m.
+    # This diagonal function is used to compute the length of the diagonal of the rectangle using the Pythagorean theorem.
+    # Example: diagonal for a rectangle(3,4) will be square(3^2 + 4^2) = 5
+    def diagonal(self):
+        return 
     
 
-    # Determine the maximum number of all the numbers
-    # in the tuple "t2" and assign the result to the variable n.
-
-
-    return x, y, z, f, m, n
-
-
-
-tuple()
+    # This is_square function is used to check if this rectangle is square, if this rectangle is square, return True, otherwise return False.
+    # Checks if the rectangle is a square (i.e., if its width and height are equal).
+    # Example: is_square for a rectangle(3,4) will be False
+    # Example: is_square for a rectangle(4,4) will be True
+    def is_square(self):
+        return 
+    
+    #This resize function is used to resize the rectangle. New_width and new_height are the new width and height.
+    def resize(self, new_width, new_height):
+        self.width = 
+        self.height = 
