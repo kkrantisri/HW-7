ac15a8dba4513c40ffc063a86fcb4834ab3841de
Kranti Sri Kalepalli <157083369+kkrantisri@users.noreply.github.com>
2024-11-13 11:01:02 -0500

    Initial commit

--- answer_test.py
+++ answer_test.py
@@ -2,7 +2,6 @@ import pytest
 import answer
 
 class TestAnswer():
-
     __correct__ = 0
     __total__ = 0
 
@@ -14,11 +13,54 @@ class TestAnswer():
 
     @classmethod
     def teardown_class(cls):
+        print(f"Score:{(cls.__correct__ / cls.__total__) * 100}%")
 
-
-        print(f"Score:{(cls.__correct__/cls.__total__)*100}%")
-        
-    def test_tuple(self):
+    def test_Q1_Rectangel_1_area(self):
         TestAnswer.__total__ += 1
-        assert(answer.tuple() == (13, 100, 7, 3, 10168, 8679)) 
+        x = answer.Rectangle(3,4)
+        result = x.area()
+        assert (result == 12)
+        TestAnswer.__correct__ += 1
+
+    def test_Q1_Rectangel_1_perimeter(self):
+        TestAnswer.__total__ += 1
+        x = answer.Rectangle(3,4)
+        result = x.perimeter()
+        assert (result == 14)
+        TestAnswer.__correct__ += 1
+
+    def test_Q1_Rectangel_1_diagonal(self):
+        TestAnswer.__total__ += 1
+        x = answer.Rectangle(3,4)
+        result = x.diagonal()
+        assert (round(result,2) == 5.0)
+        TestAnswer.__correct__ += 1
+
+    def test_Q1_Rectangel_1_diagonal(self):
+        TestAnswer.__total__ += 1
+        x = answer.Rectangle(3,4)
+        result = x.is_square()
+        assert (result == False)
+        TestAnswer.__correct__ += 1
+
+    def test_Q1_Rectangel_1_diagonal(self):
+        TestAnswer.__total__ += 1
+        x = answer.Rectangle(3,4)
+        x.resize(5,10)
+        result = x.area()
+        assert (result == 50)
+        TestAnswer.__correct__ += 1
+
+    def test_Q1_Rectangel_2_area(self):
+        TestAnswer.__total__ += 1
+        x = answer.Rectangle(10,10)
+        result = x.area()
+        assert (result == 100)
+        TestAnswer.__correct__ += 1
+
+    def test_Q1_Rectangel_2_area(self):
+        TestAnswer.__total__ += 1
+        x = answer.Rectangle(10,10)
+        result = x.is_square()
+        assert (result == True)
         TestAnswer.__correct__ += 1
